7293b3957baedf905547d07f65582101c3884e0e
kmartin-ru <i@kmartin.ru>
2022-07-28 12:43:48 +0300

    completed stylization of page-footer

---
+++ source/js/modules/accordeon.js
@@ -0,0 +1,24 @@
+const accordeon = document.querySelector('[data-el="accordeon"]');
+const accordeonTitles = accordeon.querySelectorAll('[data-el="accordeon-title"]');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener('click', function () {
+    const activeTitle = accordeon.querySelector('.is-active');
+    const activeDescr = accordeon.querySelector('.is-visible');
+    const clickedDescr = accordeonTitle.parentElement.querySelector('[data-el="accordeon-description"]');
+
+    if (activeTitle) {
+      activeTitle.classList.remove('is-active');
+    }
+
+    accordeonTitle.classList.toggle('is-active');
+
+    if (activeDescr) {
+      activeDescr.classList.remove('is-visible');
+      activeDescr.style.maxHeight = null;
+    }
+
+    clickedDescr.classList.toggle('is-visible');
+    clickedDescr.style.maxHeight = clickedDescr.scrollHeight + 'px';
+  });
+});
